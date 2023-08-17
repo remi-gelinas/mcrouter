e3078242056e3798af34e121287a44319a43fa7f
Remi Gelinas <mail@remigelin.as>
2023-08-17 10:39:41 -0400

    ci: test

--- images.nix
+++ images.nix
@@ -2,11 +2,32 @@
   self,
   system,
   pkgs,
-}: {
-  alpine = pkgs.dockerTools.streamLayeredImage {
-    name = "mcrouter";
-    tag = "${pkgs.mcrouter.version}-${self.rev}-alpine";
-    created = "now";
-    contents = [pkgs.mcrouter];
+}: let
+  mkImageFrom = let
+    inherit (pkgs) mcrouter;
+    bareTag = "${mcrouter.version}_${self.rev or "dirty"}";
+  in
+    {
+      baseImage ? null,
+      mkTag ? _: bareTag,
+    }:
+      pkgs.dockerTools.buildLayeredImage {
+        fromImage = baseImage;
+
+        name = "mcrouter";
+        tag = mkTag bareTag;
+        created = "now";
+        contents = [mcrouter];
+      };
+in {
+  mcrouter = {
+    bare = mkImageFrom {};
+    alpine = let
+      source = pkgs.sources."alpine-${system}";
+    in
+      mkImageFrom {
+        baseImage = source.src;
+        mkTag = tag: "${tag}_alpine-${source.version}";
+      };
   };
 }
